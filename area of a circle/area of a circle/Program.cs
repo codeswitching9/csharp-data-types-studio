00362dc1e87402176a008ed68858af965ed6df3e
codeswitchingstl <codeswitching@icloud.com>
2022-08-04 15:32:27 -0500

    added area of a circle section

---
+++ area of a circle/area of a circle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace area_of_a_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // calculate the area of a circle
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
+
+            // more calculations
+            double circumference = 2 * Math.PI * radius;
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + circumference);
+        }
+    }
+}
